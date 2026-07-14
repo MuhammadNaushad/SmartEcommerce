0431810a5ccba95fed50436974af3f8f733b2269
MuhammadNaushad <mnaushad173@gmail.com>
2026-07-14 12:59:20 +0530

    safe area view done

--- src/components/views/AppSafeView.tsx
+++ src/components/views/AppSafeView.tsx
@@ -1,15 +1,18 @@
-import { Platform, StatusBar, StyleSheet, View } from "react-native";
+import { StatusBar, StyleSheet, View, ViewStyle } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { AppColor } from "../../styles/colors";
+import { ReactNode } from "react";
+import { IS_ANDROID } from "../../constants/constants";
 
 interface AppSafeViewProps {
-  children: React.ReactNode;
+  children: ReactNode;
+  style: ViewStyle;
 }
 
-const AppSafeView = ({ children }: AppSafeViewProps) => {
+const AppSafeView = ({ children, style }: AppSafeViewProps) => {
   return (
     <SafeAreaView style={styles.safeArea}>
-      <View>{children}</View>
+      <View style={[styles.container, style]}>{children}</View>
     </SafeAreaView>
   );
 };
@@ -20,6 +23,9 @@ const styles = StyleSheet.create({
   safeArea: {
     flex: 1,
     backgroundColor: AppColor.white,
-    paddingTop: Platform.OS === "android" ? StatusBar.currentHeight || 0 : 0,
+    paddingTop: IS_ANDROID ? StatusBar.currentHeight || 0 : 0,
+  },
+  container: {
+    flex: 1,
   },
 });
